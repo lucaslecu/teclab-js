b00447733d9f36ead22e922be6303c87b63ee76e
lucas lecuna <lucaslecuna1986@gmail.com>
2023-07-19 20:29:58 -0300

    api3

--- js/carrito.js
+++ js/carrito.js
@@ -1,11 +1,20 @@
-const carritoFruta = []
+const almacenarCarrito = () =>{
+  if (carritoFruta.length > 0)
+  localStorage.setItem('mi carrito',JSON.stringify(carritoFruta))
+}
+const recuperarCarrito = () =>{
+  return JSON.parse( localStorage.getItem('mi carrito')) || []
+} 
+
+const carritoFruta = recuperarCarrito()
 
 let agregarAlCarrito = (frutaid) => {
     if (frutaid > 0) {
       const resultado = productos.find((producto) => producto.id === parseInt(frutaid));
       if (resultado !== undefined) {
         carritoFruta.push(resultado);
-        console.table(carritoFruta);
+        almacenarCarrito()
+        //console.table(carritoFruta);
       } else {
         console.warn('No se encontró el producto.');
       }
